3ee49b0aed464c485c77a3b9769b487a45d47156
Md Islam <mdislamshaikhm@gmail.com>
2025-10-30 15:54:16 +0530

    upload this file

---
+++ islam/islam.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter any number :"<<endl;   //islam
+    cin>>n;
+    int sum  = 10;
+    sum = sum+n;
+    cout<<"sum is :"<<sum<<endl;
+}
